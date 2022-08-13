8329055847eb1fcc3b12d989c19f31f504c1ef7c
Aarya Ahjolia <aaryaahjolia282@gmail.com>
2022-08-13 12:54:19 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "../src/CSS/Universal.css"
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import HomePage from './components/HomePage';
+import Buyers from './components/Buyers';
+import Sellers from './components/Sellers';
+import AboutUs from './components/AboutUs';
+import ErrorPage from './components/ErrorPage';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
+
+  // const [ActiveLink, setActiveLink] = useState("navbar_link_3");
+  // const [ActiveColor, setActiveColor] = useState({
+  //   color:"black"
+  // });
+
+  // const setActiveLinkFunction = (link)=>{
+  //   setActiveLink(link);
+  // }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+          <Router>
+            <ScrollToTop/>
+            <Navbar/>
+
+            <Routes>
+
+              <Route exact path="/" element={<HomePage />}></Route>
+
+              {/* <Route exact path="/WhatWeDo" element={<WhatWeDo/>}></Route> */}
+
+              <Route exact path="/Buyers" element={<Buyers/>}></Route>
+              <Route exact path="/Sellers" element={<Sellers/>}></Route>
+              <Route exact path="/AboutUs" element={<AboutUs/>}></Route>
+
+              {/* For Error 404 Page when someone enters random url */}
+              <Route path="*" element={<ErrorPage/>}></Route>
+
+            </Routes>
+
+            <Footer/>
+          </Router>
+
+    </>
   );
 }
 
